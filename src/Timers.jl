2916af5d83858284cd474c6546ee950deb657252
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-05-21 21:19:42 +0200

    Improve function wait_until()

--- src/Timers.jl
+++ src/Timers.jl
@@ -47,11 +47,18 @@ end
 end
 
 # time in seconds since epoch
-@inline function wait_until(finish)
-    delta = 0.0002
+@inline function wait_until(finish; always_sleep=false)
+    if always_sleep
+        sleep(0.001)
+    end
+    delta1 = 0.002
+    delta2 = 0.0002
+    if finish - delta1 > time()
+        sleep(finish - time() - 0.001)
+    end
     # sleep 
-    while finish - delta > time()
-        Base.Libc.systemsleep(delta)
+    while finish - delta2 > time()
+        Base.Libc.systemsleep(delta2)
     end
     # busy waiting
     while finish > time()-0.95e-6
